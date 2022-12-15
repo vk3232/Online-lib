413348f403526bdb36b9280948f0f95188763eab
vk3232 <vrushabhbk7219@gmail.com>
2022-12-15 15:31:04 +0530

    static

--- Onlinelib/settings.py
+++ Onlinelib/settings.py
@@ -132,3 +132,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = "/var/www/example.com/static/"
